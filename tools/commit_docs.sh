fc2cbc2976e4db33b47b5d6879171890f1b6a129
nikolaj <nikolajgliese@tutanota.com>
2022-04-17 19:28:38 +0200

    document update

--- tools/commit_docs.sh
+++ tools/commit_docs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # simple bash script to commit documentation updates
+# Save current working directory
+cwd=$(pwd)
 path_to_git_docs=~/Project/NGliese.github.io
 cd ${path_to_git_docs}
 git add ./
 git commit -m "document update"
 git push
+cd "$cwd"
